8567baaf0ecf4ec22b6d21ef8242c9ba67d2a75d
poshjosh <posh.bc@gmail.com>
2026-08-27 21:35:28 +0200

    chore: disable prevent_destroy for staging reprovision

--- infra/hetzner/main.tf
+++ infra/hetzner/main.tf
@@ -209,7 +209,7 @@ resource "hcloud_server" "default" {
   # To destroy a PRODUCTION server:
   #   Same procedure, plus: remove any agent nodes first.  
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
 
